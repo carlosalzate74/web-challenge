05d839f60da7d94cc21b05965a98cebfeabde8d2
Carlos Alzate <carlos-alzate@hotmail.com>
2020-02-21 20:33:47 -0600

    Running black to format

--- app.py
+++ app.py
@@ -19,38 +19,49 @@ def landing():
 def cloudiness():
     return render_template("cloudiness.html")
 
+
 @app.route("/humidity")
 def humidity():
     return render_template("humidity.html")
+
 
 @app.route("/temperature")
 def temperature():
     return render_template("temperature.html")
 
+
 @app.route("/wind")
 def wind():
     return render_template("wind.html")
+
 
 @app.route("/comparison")
 def comparison():
     return render_template("comparison.html")
 
+
 @app.route("/data")
 def data():
-	data = pd.read_csv("static/cities.csv")
-	df = data.rename(columns={"id": "City Id",
-                "name":"City",
-                "clouds.all": "Cloudiness",
-                "sys.country": "Country",
-                "dt": "Date",
-                "main.humidity": "Humidity",
-                "coord.lat": "Lat",
-                "coord.lon": "Lng",
-                "main.temp": "Max Temp",
-                "wind.speed": "Wind Speed"})
-	df = df.sort_values(by=['City Id'])
-	
-	return render_template("data.html", column_names=df.columns.values, data=list(df.values.tolist()))
+    data = pd.read_csv("static/cities.csv")
+    df = data.rename(
+        columns={
+            "id": "City Id",
+            "name": "City",
+            "clouds.all": "Cloudiness",
+            "sys.country": "Country",
+            "dt": "Date",
+            "main.humidity": "Humidity",
+            "coord.lat": "Lat",
+            "coord.lon": "Lng",
+            "main.temp": "Max Temp",
+            "wind.speed": "Wind Speed",
+        }
+    )
+    df = df.sort_values(by=["City Id"])
+
+    return render_template(
+        "data.html", column_names=df.columns.values, data=list(df.values.tolist())
+    )
 
 
 if __name__ == "__main__":
